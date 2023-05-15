119ad902c4d4ac7be708c998b167dacaf8a475a1
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2023-05-14 22:11:42 -0500

    Inicio sesion

--- app/src/main/java/com/uniandes/medisalud/MainActivity.kt
+++ app/src/main/java/com/uniandes/medisalud/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_escalas, R.id.nav_dosis, R.id.nav_otro
+                R.id.nav_escalas, R.id.nav_dosis, R.id.nav_signin
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/uniandes/medisalud/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/uniandes/medisalud/ui/slideshow/SlideshowFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.uniandes.medisalud.databinding.FragmentSlideshowBinding
+import com.uniandes.medisalud.databinding.FragmentSinginBinding
 
 class SlideshowFragment : Fragment() {
 
-    private var _binding: FragmentSlideshowBinding? = null
+    private var _binding: FragmentSinginBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,13 +25,9 @@ class SlideshowFragment : Fragment() {
         val slideshowViewModel =
             ViewModelProvider(this).get(SlideshowViewModel::class.java)
 
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        _binding = FragmentSinginBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
